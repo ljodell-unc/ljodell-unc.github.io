22328e712efd0030d72e86e5157bdbb6201b8566
Jill O'Dell <59672518+ljodell-unc@users.noreply.github.com>
2023-04-11 20:03:02 -0400

    Started adding about page animations

--- scripts/aboutAnimations.js
+++ scripts/aboutAnimations.js
@@ -11,6 +11,50 @@ tl.fromTo(".trait4", {y: 20}, {opacity: 1, y:0});
 tl.fromTo(".scrollArrow1", {}, {duration: 3, opacity: 1});
 tl.fromTo(".scrollArrow2", {}, {duration: 3, opacity: 1, onComplete: move(".scrollArrow1", ".scrollArrow2")}, "<");
 
+ScrollTrigger.defaults({scroller:".content"});
+
+// Photo Gallery Animations (fade in and swing)
+gsap.set(".photoGallery", {y: 20});
+gsap.set(".soundPhoto", {y: 20, rotation: 90});
+gsap.set(".beachPhoto", {y: 20, rotation: 90});
+gsap.set(".beachPhoto2", {y: 20, rotation: 90});
+
+gsap.to(".photoGallery", {scrollTrigger: {
+    trigger: ".section2",
+    toggleActions: "restart pause restart pause"},
+    immediateRender: false,
+    duration: 1.5,
+    opacity: 1,
+    y: 0});
+
+gsap.to(".soundPhoto", {scrollTrigger: {
+    trigger: ".section2",
+    toggleActions: "restart pause restart pause"},
+    immediateRender: false,
+    duration: 2,
+    transformOrigin: "top 50%",
+    rotation: 0,
+    ease: Elastic.easeOut.config(1, 0.3)});
+
+gsap.to(".beachPhoto", {scrollTrigger: {
+    trigger: ".section2",
+    toggleActions: "restart pause restart pause"},
+    immediateRender: false,
+    duration: 2.5,
+    transformOrigin: "top 50%",
+    rotation: 0,
+    ease: Elastic.easeOut.config(1, 0.3)});
+
+gsap.to(".beachPhoto2", {scrollTrigger: {
+    trigger: ".section2",
+    toggleActions: "restart pause restart pause"},
+    immediateRender: false,
+    duration: 2.3,
+    transformOrigin: "top 50%",
+    rotation: 0,
+    ease: Elastic.easeOut.config(1, 0.3)});
+
+
 /* handles arrow movement animation; called upon 2nd arrow fade in completion*/
 function move(arrow1, arrow2) {
     gsap.to(arrow1, {duration: 0.5, y:10, yoyo: true, repeat: -1, repeatDelay: 0.5});
